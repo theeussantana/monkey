39d9fa193ce603866fa5f5496dd21562d524fda8
TheeusSantana <matheusmelo2803@gmail.com>
2024-01-19 22:45:44 -0300

    First commit

---
+++ backend/src/services/QueueOptionService/ListService.ts
@@ -0,0 +1,36 @@
+import { WhereOptions } from "sequelize/types";
+import QueueOption from "../../models/QueueOption";
+
+type QueueOptionFilter = {
+  queueId: string | number;
+  queueOptionId: string | number;
+  parentId: string | number | boolean;
+};
+
+const ListService = async ({ queueId, queueOptionId, parentId }: QueueOptionFilter): Promise<QueueOption[]> => {
+
+  const whereOptions: WhereOptions = {};
+
+  if (queueId) {
+    whereOptions.queueId = queueId;
+  }
+
+  if (queueOptionId) {
+    whereOptions.id = queueOptionId;
+  }
+
+  if (parentId === -1) {
+    whereOptions.parentId = null;
+  } else if (typeof parentId === "number" && parentId > 0) {
+    whereOptions.parentId = parentId;
+  }
+
+  const queueOptions = await QueueOption.findAll({
+    where: whereOptions,
+    order: [["id", "ASC"]]
+  });
+
+  return queueOptions;
+};
+
+export default ListService;
